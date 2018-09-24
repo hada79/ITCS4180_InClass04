8860bccd45b3dca67361ce1dee9502fb592fb960
Thomas <tgeddes@uncc.edu>
2018-09-24 19:52:10 -0400

    Added alert dialog.
    Added alertdialog, changed image to be empty white image, added connection to internet in manifest.

--- app/src/main/java/com/jasonhada/inclass04/MainActivity.java
+++ app/src/main/java/com/jasonhada/inclass04/MainActivity.java
@@ -1,13 +1,115 @@
 package com.jasonhada.inclass04;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    String[] keywords;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (isConnected()){
+            new GetKeywordsAsync().execute();
+
+            findViewById(R.id.search_btn).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                    alert.setTitle("Choose a Keyword").setItems(keywords, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            EditText keyword = findViewById(R.id.search_et);
+                            keyword.setText(keywords[which]);
+                        }
+                    });
+                    alert.create();
+                    alert.show();
+                }
+            });
+        }else{
+            Toast.makeText(MainActivity.this, "Disconnected", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private boolean isConnected(){
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if(networkInfo == null || !networkInfo.isConnected()){
+            return false;
+        }
+
+        return true;
+    }
+
+    private class GetKeywordsAsync extends AsyncTask<Void, Void, String>{
+        @Override
+        protected String doInBackground(Void... params) {
+            StringBuilder stringBuilder = new StringBuilder();
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            String result = null;
+
+            try {
+                URL url = new URL("http://dev.theappsdr.com/apis/photos/keywords.php");
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String line = "";
+                    while ((line = reader.readLine()) != null) {
+                        stringBuilder.append(line);
+                    }
+                    result = stringBuilder.toString();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(connection != null){
+                    connection.disconnect();
+                }
+
+                if (reader != null){
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s != null){
+                keywords = s.split(";");
+            }else{
+                Toast.makeText(MainActivity.this, "No keywords found", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
